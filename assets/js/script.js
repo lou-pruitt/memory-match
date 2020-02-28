a4a51fbf83f777ad8e4fc0a7a42e75fcd9001ee3
Lou Pruitt <Lpruitt2004@gmail.com>
2020-02-27 16:41:07 -0800

    Added card matching

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,8 @@
 var gameCards = document.getElementById('gameCards');
+var firstCardClicked;
+var secondCardClicked;
+var firstCardClasses;
+var secondCardClasses;
 
 gameCards.addEventListener('click', handleClick);
 
@@ -6,6 +10,30 @@ function handleClick(event) {
   if (event.target.className.indexOf('card-back') === -1) {
     return;
   }
+
   var cardClicked = event.target;
+
   cardClicked.classList.add('hidden');
+
+  if (!firstCardClicked) {
+    firstCardClicked = cardClicked;
+    firstCardClasses = firstCardClicked.previousElementSibling.className;
+  } else {
+    secondCardClicked = cardClicked;
+    secondCardClasses = secondCardClicked.previousElementSibling.className;
+    gameCards.removeEventListener('click', handleClick);
+    if (firstCardClasses === secondCardClasses) {
+      gameCards.addEventListener('click', handleClick);
+      firstCardClicked = null;
+      secondCardClicked = null;
+    } else {
+      setTimeout(function() {
+        firstCardClicked.classList.remove('hidden');
+        secondCardClicked.classList.remove('hidden');
+        gameCards.addEventListener('click', handleClick);
+        firstCardClicked = null;
+        secondCardClicked = null;
+      }, 1500);
+    }
+  }
 }
