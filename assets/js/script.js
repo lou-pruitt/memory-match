454ca3fbeb043c39a6ac55e3408bb3d102039423
Lou Pruitt <Lpruitt2004@gmail.com>
2020-02-29 19:24:01 -0800

    Add reset game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 var gameCards = document.getElementById('gameCards');
+var resetBtn = document.getElementById('resetBtn');
 var firstCardClicked;
 var secondCardClicked;
 var firstCardClasses;
@@ -9,6 +10,7 @@ var attempts = 0;
 var gamesPlayed = 0;
 
 gameCards.addEventListener('click', handleClick);
+resetBtn.addEventListener('click', resetGame);
 
 function handleClick(event) {
   if (event.target.className.indexOf('card-back') === -1) {
@@ -61,5 +63,27 @@ function displayStats() {
 }
 
 function calculateAccuracy(attempts, matches) {
+  if (!attempts) {
+    return '0';
+  }
   return Math.trunc((matches / attempts) * 100);
+}
+
+function resetGame() {
+  matches = 0;
+  attempts = 0;
+  gamesPlayed++;
+  displayStats();
+  resetCards();
+  var modal = document.querySelector('.modal');
+  modal.classList.add('hidden');
+}
+
+function resetCards() {
+  var hiddenCards = document.querySelectorAll('.card-back');
+  console.log('hiddenCards', hiddenCards.length);
+  let i;
+  for (i = 0; i < hiddenCards.length; i++) {
+    hiddenCards[i].classList.remove('hidden');
+  }
 }
